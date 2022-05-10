8c5b4b61d9bb0f2c46e6ba89a7400fd233fd8fff
Utsho Das <Utsho Das@Laptop>
2022-05-10 12:31:19 +0600

    Added Simple Menu

---
+++ Ride With The Wave/src/model/WaveButton.java
@@ -0,0 +1,89 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.text.Font;
+
+public class WaveButton extends Button {
+	
+	private final String FONT_PATH = "src/model/resources/kenney_pixel.ttf";
+	
+	public WaveButton( String text) {
+		setText(text);
+		setButtonFont();
+		setPrefWidth(190);
+		setPrefHeight(50);
+		initializeMouseListners();
+		
+	}	
+	private void setButtonPressedStyle()
+	{
+		setPrefHeight(45);
+		setLayoutY(getLayoutY()+4);
+	}
+	private void setButtonReleasedStyle()
+	{
+		setPrefHeight(49);
+		setLayoutY(getLayoutY()-4);
+	}
+	private void setButtonFont()
+	{
+		try {
+			setFont(Font.loadFont( new FileInputStream(FONT_PATH), 30));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			setFont(Font.font("AvantGarde",23));
+		}
+	}
+	private void initializeMouseListners()
+	{
+		setOnMousePressed(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				if( event.getButton().equals(MouseButton.PRIMARY))
+					setButtonPressedStyle();	
+			}	
+		});
+		
+		setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				if( event.getButton().equals(MouseButton.PRIMARY))
+					setButtonReleasedStyle();	
+			}	
+		});
+		
+		setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+			setEffect( new DropShadow());
+			}
+		});
+		setOnMouseExited( new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				setEffect(null);
+				
+			}
+			
+		});
+	}
+	
+	
+
+}
--- Ride With The Wave/src/view/ViewManager.java
+++ Ride With The Wave/src/view/ViewManager.java
@@ -1,16 +1,32 @@
 package view;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
 import javafx.stage.Stage;
+import model.WaveButton;
 
 public class ViewManager {
 
+	
 	public static final int HEIGHT = 600;
 	public static final int WIDTH = 800;
 	private AnchorPane mainPane;
 	private Scene mainScene;
 	private Stage mainStage;
+	
+	private final static int MENU_BUTTON_START_X = 300;
+	private final static int MENU_BUTTON_START_Y =100;
+	
+	List <WaveButton> menuButtons;
 	
 	public ViewManager()
 	{
@@ -18,11 +34,41 @@ public class ViewManager {
 		mainScene = new Scene(mainPane, WIDTH,HEIGHT);
 		mainStage = new Stage();
 		mainStage.setScene(mainScene);
+		menuButtons = new ArrayList<>();
+		createMenuButton("PLAY!");
+		createMenuButton("Scores");
+		createMenuButton("About");
+		createMenuButton("Exit");
 		
+		createBackground();
 	}
 	
+	private void createBackground() {
+		Image backgroundImage = new Image(getClass().getResource("blue_panel.jpg").toExternalForm(), 256, 256, false, true);
+		BackgroundImage background = new BackgroundImage(backgroundImage, BackgroundRepeat.REPEAT,BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+		mainPane.setBackground(new Background(background));
+		
+	}
+
 	public Stage getMainStage()
 	{
 		return mainStage;
+	}
+	
+	private void  addMenuButton(WaveButton button) {
+		button.setLayoutX(MENU_BUTTON_START_X);
+		button.setLayoutY(MENU_BUTTON_START_Y+menuButtons.size()*100);
+		menuButtons.add(button);
+		mainPane.getChildren().add(button);
+		
+		
+	}
+	private void  createMenuButton(String text) {
+		
+		WaveButton button = new WaveButton(text);
+		addMenuButton(button);
+		BackgroundImage backgroundImage = new BackgroundImage( new Image( getClass().getResource("red_button.png").toExternalForm()), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,null);
+        Background background = new Background(backgroundImage);
+        button.setBackground(background);
 	}
 }
