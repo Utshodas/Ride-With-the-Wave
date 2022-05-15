e7346c583a5b906169c8c0aa71d26458b75bee49
Utsho Das <Utsho Das@Laptop>
2022-05-15 23:15:05 +0600

    Added the boy and movement

--- Ride With The Wave/src/view/GameViewManager.java
+++ Ride With The Wave/src/view/GameViewManager.java
@@ -2,8 +2,11 @@ package view;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -17,6 +20,13 @@ public class GameViewManager {
 	
 	public static final int GAME_HEIGHT = 603;
 	public static final int GAME_WIDTH = 1071;
+	
+	ImageView boy;
+	
+	private boolean isDownKeyPressed;
+	private boolean isUpKeyPressed;
+	private int angle; //will add some rotating animation later
+	private AnimationTimer gameTimer;
 
 	public GameViewManager() {
 		initializeStage();
@@ -30,32 +40,17 @@ public class GameViewManager {
 
 			@Override
 			public void handle(KeyEvent event) {
-				if(event.getCode()==KeyCode.LEFT)
+				if(event.getCode()==KeyCode.UP)
 				{
+					isUpKeyPressed=true;
 					
-				}else if (event.getCode()==KeyCode.RIGHT)
+				}else if (event.getCode()==KeyCode.DOWN)
 				{
-					
+					isDownKeyPressed=true;
 				}
 				
 				
 			}
-		});
-		gameScene.setOnKeyReleased( new EventHandler <KeyEvent>() {
-
-			@Override
-			public void handle(KeyEvent event) {
-				if(event.getCode()==KeyCode.LEFT)
-				{
-					
-				}else if (event.getCode()==KeyCode.RIGHT)
-				{
-					
-				}
-		
-				
-			}
-			
 			
 		});
 	}
@@ -65,6 +60,7 @@ public class GameViewManager {
 		gamePane = new AnchorPane();
 		gameScene = new Scene(gamePane,GAME_WIDTH,GAME_HEIGHT);
 		gameStage = new Stage();
+		gameStage.setResizable(false);
 		gameStage.setScene(gameScene);
 		
 	}
@@ -72,7 +68,48 @@ public class GameViewManager {
 	public void createNewGame(Stage menuStage) {
 		this.menuStage = menuStage;
 		this.menuStage.hide();
+		createBoy();
+		gameLoop();
 		gameStage.show();
+	}
+	
+	private void createBoy() {
+		boy = new ImageView(new Image(getClass().getResourceAsStream("sufer_boy_small.png")));
+		boy.setLayoutX(10);
+		boy.setLayoutY(50);
+		gamePane.getChildren().add(boy);
+		}
+	
+	private void gameLoop()
+	{
+		gameTimer= new AnimationTimer() {
+
+			@Override
+			public void handle(long arg0) {
+				// TODO Auto-generated method stub
+				moveBoy();
+			}
+		};
+		gameTimer.start();
+	}
+	
+	private void moveBoy() {
+		if(isDownKeyPressed && !isUpKeyPressed)
+		{
+			if(boy.getLayoutY()<450) {
+				boy.setLayoutY(boy.getLayoutY()+200);
+			}
+			isDownKeyPressed=false;
+			
+		}
+		if(!isDownKeyPressed && isUpKeyPressed)
+		{
+			if(boy.getLayoutY()>50) {
+				boy.setLayoutY(boy.getLayoutY()-200);
+			}
+			isUpKeyPressed=false;
+		}
+		
 	}
 
 }
